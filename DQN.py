36ca30eccd0dadb5fa88d2646800523e4c5da5f1
Alvaro Queiroz <alvaroqueiroz.mas@gmail.com>
2019-04-24 21:30:12 -0300

    Implemented class Agent
    method to create Residual CNN with dropout implemented

---
+++ DQN.py
@@ -0,0 +1,145 @@
+from keras import layers
+from keras import models
+from keras.optimizers import adam
+import tensorflow as tf
+from collections import deque
+import random
+import numpy as np
+class Agent:
+    def __init__(self, state_size, action_size):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.memory = deque(maxlen=2000) # double-ended queue; acts like list, but elements can be added/removed from either end
+        self.gamma = 0.95 # decay or discount rate: enables agent to take into account future actions in addition to the immediate ones, but discounted at this rate
+        self.epsilon = 1.0 # exploration rate: how much to act randomly; more initially than later due to epsilon decay
+        self.epsilon_decay = 0.995 # decrease number of random explorations as the agent's performance (hopefully) improves over time
+        self.epsilon_min = 0.01 # minimum amount of random exploration permitted
+        self.learning_rate = 0.001 # rate at which NN adjusts models parameters via SGD to reduce cost 
+        self.model = self._build_model() # private method 
+    
+    def _build_model(self):
+        cardinality = 1
+        def residual_network(x):
+            """
+            ResNeXt by default. For ResNet set `cardinality` = 1 above.
+            
+            """
+            def add_common_layers(y):
+                y = layers.BatchNormalization()(y)
+                y = layers.ReLU()(y)
+
+                return y
+
+            def grouped_convolution(y, nb_channels, _strides):
+                # when `cardinality` == 1 this is just a standard convolution
+                if cardinality == 1:
+                    return layers.Conv2D(nb_channels, kernel_size=(3, 3), strides=_strides, padding='same')(y)
+                
+                assert not nb_channels % cardinality
+                _d = nb_channels // cardinality
+
+                # in a grouped convolution layer, input and output channels are divided into `cardinality` groups,
+                # and convolutions are separately performed within each group
+                groups = []
+                for j in range(cardinality):
+                    group = layers.Lambda(lambda z: z[:, :, :, j * _d:j * _d + _d])(y)
+                    groups.append(layers.Conv2D(_d, kernel_size=(3, 3), strides=_strides, padding='same')(group))
+                    
+                # the grouped convolutional layer concatenates them as the outputs of the layer
+                y = layers.concatenate(groups)
+
+                return y
+
+            def residual_block(y, nb_channels_in, nb_channels_out, _strides=(1, 1), _project_shortcut=False):
+                """
+                Our network consists of a stack of residual blocks. These blocks have the same topology,
+                and are subject to two simple rules:
+                - If producing spatial maps of the same size, the blocks share the same hyper-parameters (width and filter sizes).
+                - Each time the spatial map is down-sampled by a factor of 2, the width of the blocks is multiplied by a factor of 2.
+                """
+                shortcut = y
+
+                # we modify the residual building block as a bottleneck design to make the network more economical
+                y = layers.Conv2D(nb_channels_in, kernel_size=(1, 1), strides=(1, 1), padding='same')(y)
+                y = add_common_layers(y)
+
+                # ResNeXt (identical to ResNet when `cardinality` == 1)
+                y = grouped_convolution(y, nb_channels_in, _strides=_strides)
+                y = add_common_layers(y)
+
+                y = layers.Conv2D(nb_channels_out, kernel_size=(1, 1), strides=(1, 1), padding='same')(y)
+                # batch normalization is employed after aggregating the transformations and before adding to the shortcut
+                y = layers.BatchNormalization()(y)
+
+                # identity shortcuts used directly when the input and output are of the same dimensions
+                if _project_shortcut or _strides != (1, 1):
+                    # when the dimensions increase projection shortcut is used to match dimensions (done by 1×1 convolutions)
+                    # when the shortcuts go across feature maps of two sizes, they are performed with a stride of 2
+                    shortcut = layers.Conv2D(nb_channels_out, kernel_size=(1, 1), strides=_strides, padding='same')(shortcut)
+                    shortcut = layers.BatchNormalization()(shortcut)
+
+                y = layers.add([shortcut, y])
+
+                # relu is performed right after each batch normalization,
+                # expect for the output of the block where relu is performed after the adding to the shortcut
+                y = layers.ReLU()(y)
+
+                return y
+
+            # conv1
+            x = layers.Conv2D(64, kernel_size=(7, 7), strides=(2, 2), padding='same')(x)
+            x = add_common_layers(x)
+
+            # conv2
+            x = layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2), padding='same')(x)
+            for i in range(2):
+                project_shortcut = True if i == 0 else False
+                x = residual_block(x, 128, 256, _project_shortcut=project_shortcut)
+
+            x = layers.GlobalAveragePooling2D()(x)
+            x = layers.Dense(1024)(x)
+            x = layers.Dropout(0.5)(x)
+            x = layers.Dense(512)(x)
+            x = layers.Dropout(0.5)(x)
+            x = layers.Dense(self.action_size)(x)
+
+            return x
+        image_tensor = layers.Input(shape=(self.state_size))
+        network_output = residual_network(image_tensor)
+        
+        model = models.Model(inputs=[image_tensor], outputs=[network_output])
+
+        optimizer = adam(lr = self.learning_rate)
+
+        model.compile(optimizer=optimizer, 
+                    loss='mean_squared_error',
+                    metrics=['mae'])
+        return model
+    
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done)) # list of previous experiences, enabling re-training later
+
+    def act(self, state):
+        if np.random.rand() <= self.epsilon: # if acting randomly, take random action
+            return random.randrange(self.action_size)
+        act_values = self.model.predict(state) # if not acting randomly, predict reward value based on current state
+        return np.argmax(act_values[0]) # pick the action that will give the highest reward (i.e., go left or right?)
+
+    def replay(self, batch_size): # method that trains NN with experiences sampled from memory
+        minibatch = random.sample(self.memory, batch_size) # sample a minibatch from memory
+        for state, action, reward, next_state, done in minibatch: # extract data for each minibatch sample
+            target = reward # if done (boolean whether game ended or not, i.e., whether final state or not), then target = reward
+            if not done: # if not done, then predict future discounted reward
+                target = (reward + self.gamma * # (target) = reward + (discount rate gamma) * 
+                          np.amax(self.model.predict(next_state)[0])) # (maximum target Q based on future action a')
+            target_f = self.model.predict(state) # approximately map current state to future discounted reward
+            target_f[0][action] = target
+            self.model.fit(state, target_f, epochs=1, verbose=0) # single epoch of training with x=state, y=target_f; fit decreases loss btwn target_f and y_hat
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+    def load(self, name):
+        self.model.load_weights(name)
+
+    def save(self, name):
+        self.model.save_weights(name)
